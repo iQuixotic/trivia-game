df32b72d0f4c6a1b885af949677dabb4eb1f34f1
Trey <treyw1850@gmail.com>
2018-04-04 06:32:30 -0700

    working on timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 
-
+var correctCount = 0;
+var incorrectCount = 0;
+var correctAnswer;
 var j;
 var btn1 = document.getElementById('bt1');
 var btn2 = document.getElementById('bt2');
@@ -11,53 +13,109 @@ var questionArr = [
 ///---------------------------------------------------------------
    {
        number:1,
-        q: "how is this working ?",
-        choiceA: "This is choice A",
-        choiceB: "This is choice B",
-        choiceC: "This is choice C",
-        choiceD: "This is choice A"
+        q: "A cat can jump up to ______ times its length.",
+        choices: ['6', '7', '9', '10'], 
+        correct: "good"
     }, {
         number:2,
-        q: "2how is this working ?",
-        choiceA: "2This is choice A",
-        choiceB: "2This is choice B",
-        choiceC: "2This is choice C",
-        choiceD: "2This is choice A"
+        q: "Cats sleep how many hours a day on average?",
+        choices: ['up to 12', 'up to 16', 'up to 20', 'cats do not sleep during the day, they only sleep at night'], 
+        correct: 'up to 16'
     }, {
         number:3,
-        q: "3how is this working ?",
-        choiceA: "3This is choice A",
-        choiceB: "3This is choice B",
-        choiceC: "3This is choice C",
-        choiceD: "3This is choice A"
+        q: "On average, how many whiskers does a cat have?",
+        choices: ['12', '24', '30', '46'], 
+        correct: '24'
+    }, {
+        number:4,
+        q: "On average, long do outdoor cats live?",
+        choices: ['5 to 6', '3 to 5', '12 to 15', 'infinity'], 
+        correct: '3 to 5'
+    }, {
+        number:5,
+        q: "What does oscilla mean in latin?",
+        choices: ['grace', 'good kitty', 'to swing', 'little face'], 
+        correct: 'little face'
+    }, {
+        number:6,
+        q: "What do you call a group of kittens?",
+        choices: ['kroger', 'kaggle', 'kindle', 'kaboodle'], 
+        correct: 'kindle'
+    }, {
+        number:7,
+        q: "What is the term for when a cat rubs the side of its head against people or furniture?",
+        choices: ['kroger', 'tagging', 'beaning', 'bunting'], 
+        correct: 'bunting'
+    }, {
+        number:8,
+        q: "Cats cannot perceive which of the following taste?",
+        choices: ['sour', 'salty', 'sweet', 'icy-hot'], 
+        correct: 'sweet'
+    }, {
+        number:9,
+        q: "Cats or dogs?",
+        choices: ['cats', 'dogs', 'other answer', 'snakes'], 
+        correct: 'cats'
+    }, {
+        number:10,
+        q: "4000 years ago in egypt, what was the penalty for killing a cat?",
+        choices: ['not death', 'death', 'not death', 'not death'], 
+        correct: 'death'
     }
 ];
 
 questionRandomizer();
 
-var str, htmla1, htmla2, htmla3, htmla4;
+
+//======================================================================
+//main loop (maybe in a start game function)
+for (i = 0; i < newOrderArr.length; i++) {
+   // countdown();
+    if (newOrderArr[i].choices === newOrderArr[i].correct) {
+        correctCount++;             //increment count of correct
+        changeQ();
+    } else { incorrectCount++ }     //increment count of incorrect
+    changeQ();                      //running changeQ after a wrong guess
+}
+//======================================================================
+//Part of the timer to display 20 seconds on the screen, make them decrement
+//once each second, 
+//var timeLeft = `<p>${20}</p>`
+var timeOnScreen = document.getElementById('timer');
+var timer = setInterval(countdown, 1000);
+document.getElementById('timer').innerHTML =  `<p>${20}</p>`;
+// function countdown() {
+//   if (timeLeft == 0) {
+//     clearTimeout(timeOnScreen);
+//    // youLose();
+//   } else {
+//     document.getElementById('timer').innerHTML =  `<p>${20}</p>`;
+//   //  timeLeft--;
+//   }
+// }
 // function that loops through the questions and prints them onto the jumbotron
 // part 2 of this function assigns the buttons to possible choices for that question
+var str, htmla1, htmla2, htmla3, htmla4;
 function changeQ() {
     for (i = 0; i < newOrderArr.length; i++) {
         var htmlq = `<p>${newOrderArr[i].q}</p>`
         document.getElementById("changing-question").innerHTML = htmlq;
         //this part of the functions changes each of the answer's inner text 
         // and values
-        htmla1 = `<p>${newOrderArr[i].choiceA}</p>`
+        htmla1 = `<p>${newOrderArr[i].choices[0]}</p>`
         document.getElementById("bt1").innerHTML = htmla1;
-        btn1 = newOrderArr[i].choiceA;
-       //------------------------------
-        btn2 = newOrderArr[i].choiceB;
-        htmla2 = `<p>${newOrderArr[i].choiceB}</p>`
+        btn1 = newOrderArr[i].choices[0];
+       //------------------------------ 
+        btn2 = newOrderArr[i].choices[1];
+        htmla2 = `<p>${newOrderArr[i].choices[1]}</p>`
         document.getElementById("bt2").innerHTML = htmla2;
         //---------------------------------------
-        btn3 = newOrderArr[i].choiceC;
-        htmla3 = `<p>${newOrderArr[i].choiceC}</p>`
+        btn3 = newOrderArr[i].choices[2];
+        htmla3 = `<p>${newOrderArr[i].choices[2]}</p>`
         document.getElementById("bt3").innerHTML = htmla3;
         //------------------------------------------
-        btn4 = newOrderArr[i].choiceD;
-        htmla4 = `<p>${newOrderArr[i].choiceD}</p>`
+        btn4 = newOrderArr[i].choices[3];
+        htmla4 = `<p>${newOrderArr[i].choices[3]}</p>`
         document.getElementById("bt4").innerHTML = htmla4;
     }
 }
@@ -66,7 +124,7 @@ changeQ();
 
 // make function to randomize question order
 function questionRandomizer() {
-    for (i = 0; i <= questionArr.length+1; i++) {
+    for (i = 0; i <10; i++) {
              j = Math.floor(Math.random() * questionArr.length);
              var question = questionArr[j];
              console.log(question);
@@ -75,12 +133,69 @@ function questionRandomizer() {
     }
 };
 
+function youLose() {
+    console.log("you ran out of time")
+}
 // var node = document.getElementsByClassName("myList2").lastChild;
 // document.getElementById("myList1").appendChild(node);
 
-
-
-
+//need to run this function every time an answer is selected to check for correctness
+// function switches() {
+//     switch (newOrderArr.number) {
+//         case 1:
+//         currentAnswerPer = "";
+//             if (currentAnswerPer == newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 2:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 3:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 4:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 5:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 6:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 7:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 8:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 9:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         case 10:
+//         currentAnswerPer = ""
+//             if (currentAnswerPer === newOrderArr.ChoiceC) { correctCount++; }
+//             else { incorrectCount++ }
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
 
 
