3e8342b8374372e1ef397e00a9ad44a9bd5ea6b5
Trey <treyw1850@gmail.com>
2018-04-04 22:45:52 -0700

    got all 10 questions running

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,17 +70,24 @@ var questionArr = [
 ];
 // puts the questions in a random order for the first time
 questionRandomizer();
-// makes the timer start for the first time
-runTimer();
+// makes the timer be off until game is started
+clearInterval(timer);
 
 var str, htmla1, htmla2, htmla3, htmla4;
-
+var tracker=-1;
 // for loop to pass by each question
-function changeArr(){
-    questionArr.push(newOrderArr[0]);
-    newOrderArr.splice(0, 1);
-    changeQ();
-}
+function changeArr() {
+    if(tracker >7){
+        clearInterval(timer);
+        timeOnScreen.innerHTML = timeLeft;
+    }
+    if(newOrderArr[1]){
+    newOrderArr.shift();
+    changeQ();}
+    
+    console.log(newOrderArr.length)
+    }
+
 //function for changing questions (logic only) (will run inside another function)
 function changeQ() {
     var htmlq = `<p>${newOrderArr[0].q}</p>`
@@ -89,28 +96,28 @@ function changeQ() {
     // and values
     htmla1 = `<p>${newOrderArr[0].choices[0]}</p>`
     document.getElementById("bt1").innerHTML = htmla1;
-    btn1 = newOrderArr[0].choices[0];
+   // btn1 = newOrderArr[0].choices[0];
     //-------------------------------------------
-    btn2 = newOrderArr[0].choices[1];
+    //btn2 = newOrderArr[0].choices[1];
     htmla2 = `<p>${newOrderArr[0].choices[1]}</p>`
     document.getElementById("bt2").innerHTML = htmla2;
     //---------------------------------------
-    btn3 = newOrderArr[0].choices[2];
+    // btn3 = newOrderArr[0].choices[2];
     htmla3 = `<p>${newOrderArr[0].choices[2]}</p>`
     document.getElementById("bt3").innerHTML = htmla3;
     //------------------------------------------
-    btn4 = newOrderArr[0].choices[3];
+    //btn4 = newOrderArr[0].choices[3];
     htmla4 = `<p>${newOrderArr[0].choices[3]}</p>`
     document.getElementById("bt4").innerHTML = htmla4;
 }
 // function to randomize question order
 function questionRandomizer() {
-    for (i = 0; i <10; i++) {
-             j = Math.floor(Math.random() * questionArr.length);
-             var question = questionArr[j];
-             console.log(question);
-             newOrderArr.push(question);
-             questionArr.splice([j], 1);
+    for (i = 0; i < 10; i++) {
+        j = Math.floor(Math.random() * questionArr.length);
+        var question = questionArr[j];
+        console.log(question);
+        newOrderArr.push(question);
+        questionArr.splice([j], 1);
     }
 };
 //---------------- FUNCTIONS FOR TIMER ----------------------------
@@ -119,18 +126,22 @@ function questionRandomizer() {
 var timeLeft = 20;
 var timeOnScreen = document.getElementById('timer');
 var timer;
+var tracker2 = 0;
 //function that sets up the timer to run
-function runTimer(){
+function runTimer() {
     timer = setInterval(countdown, 1000);
 }
 //function that counts down until timer equals 0
 function countdown() {
-   if(timeLeft !== 0){
-    timeLeft--;
-    timeOnScreen.innerHTML = timeLeft;
-   }if (timeLeft === 0) {
-   timerReset();
-   changeArr();
+    if (timeLeft !== 0) {
+        timeLeft--;
+        timeOnScreen.innerHTML = timeLeft;
+    } if (timeLeft === 0) {
+        timerReset();
+        if(tracker2===0){
+            changeQ();
+        }if(tracker2!==0){changeArr();}
+        tracker2++;
     }
 }
 // resets the timer and alerts time is up
@@ -138,6 +149,16 @@ function timerReset() {
     clearInterval(timer);
     timeLeft = 20;
     runTimer();
+}
+function start() {
+    clearInterval(timer);
+    timeLeft = 3;
+    timeOnScreen.innerHTML = timeLeft;
+    runTimer();
+}
+// reset game button
+function reset() {
+    location.reload();
 }
 //------------- END OF TIMER FUNCTIONS -----------------------------
 //------------- BEGINNING OF BUTTON FUNCTIONS ----------------------
